2a3592d94421625ec2b3f1f670598e7e29a021bf
AleksanderAleksandrovich <aleksveles228@gmail.com>
2023-10-05 17:44:33 +0300

    fixed vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 export default defineConfig({
+  base: '/evogym',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
